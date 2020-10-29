317bcd00bc70c76d070b25b52540e4503a8179bb
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-10-29 11:07:06 +0100

    First Commit

---
+++ HangedMan_client.c
@@ -0,0 +1,9 @@
+// HangedMan Game Client
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("Usage: %s <SERVER-IP-ADDRESS>", argv[0]);
+  }
+}
---
+++ HangedMan_server.c
@@ -0,0 +1,9 @@
+// HangedMan Game Server
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    printf("Usage: %s <SERVER-IP-ADDRESS>", argv[0]);
+  }
+}
