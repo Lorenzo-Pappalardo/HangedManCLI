1ea38b72ffee011838b444ac964c9f04fce3d107
Lorenzo Pappalardo <lorenzopappalardo7@gmail.com>
2020-10-31 12:04:24 +0100

    Renamed file

--- server.c
+++ server.c
@@ -7,36 +7,25 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#define MAXBUF 30
+
 int main(int argc, char* argv[]) {
   if (argc < 2) {
     printf("Usage: %s <SERVER-IP-ADDRESS>", argv[0]);
     exit(1);
   }
 
-  int words_fd;
-  if ((words_fd = open("words.txt", O_RDONLY)) == -1) {
-    perror("Failed to open words file");
-    exit(2);
+  FILE* input;
+  if ((input = fopen("words.txt", "r")) == NULL) {
+    perror("Error opening source file\n");
+    exit(EXIT_FAILURE);
   }
 
-  const short dim = 7776;
-  char* words[dim];
-
-  for (int i = 0; i < dim; i++) {
-    char* word = "";
-    while (1) {
-      char tmpChar;
-      if (read(words_fd, &tmpChar, sizeof(char)) == -1) {
-        perror("Read failed");
-        exit(3);
-      }
-
-      printf("%c", tmpChar);
-    }
-    words[i] = word;
+  char buffer[MAXBUF];
+  while (fgets(buffer, MAXBUF, input)) {  // fgets insert the row content in
+                                          // "buffer", with \n in the end..->
+    printf("%s", buffer);
   }
-
-  for (int i = 0; i < dim; i++) {
-    printf("%s\n", words[i]);
-  }
+  printf("\n");  //<-...except for the final string, because there's directly
+                 // EOF that interrupts the string.
 }
